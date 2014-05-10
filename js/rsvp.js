6c69772c6fee9d4e2cea472f43e828bbcfa39f13
Jason Gordon <jag2136@gmail.com>
2014-05-10 18:30:06 -0400

    Added guest lookup to RSVP form

--- js/rsvp.js
+++ js/rsvp.js
@@ -14,14 +14,13 @@ var passcodeSubmit  = function() {
         url:        rsvpReqUrl,
         data:       {passcode: passcode},
         success:    function(response) {
-            console.log(response);
-
             respElem.empty().append(response);
 
             if( response.indexOf("errText") == -1 )
             {
                 // Success, fade out passcode form and fade in rsvp form
                 $("#passcodeForm").fadeOut(duration, function() {
+                    reqFormJs();
                     respElem.fadeIn(duration);
                 });
             }
@@ -35,6 +34,95 @@ var passcodeSubmit  = function() {
 };
 
 
+var reqFormJs       = function() {
+    // Store common DOM selectors to avoid re-selecting multiple times
+    var lastNameEntry   = $("#lastNameEntry");
+    var firstNameEntry  = $("#firstNameEntry");
+
+    // Parse JSON guest data in guestData array
+    var allGuests   = [];
+    var lastNames   = [];
+
+    guestData.forEach(function(group) {
+        group.forEach(function(guest) {
+
+            if( guest.hasOwnProperty("l") &&
+                guest.l !== "" &&
+                guest.l !== "One" )
+            {
+                allGuests.push(guest);
+                if( $.inArray(guest.l, lastNames) < 0 )
+                {
+                    lastNames.push(guest.l);
+                }
+            }
+        });
+    });
+
+
+    ////////////////////////////////////////////////////
+    // Event handlers
+
+    // Fires/processes when enter is hit in the last name field
+    lastNameEntry.on("keydown", function(e) {
+        if( e.keyCode == 13 )
+        {
+            var lastNameParent = lastNameEntry.parent();
+            lastNameParent.find($(".errText")).remove();
+
+            firstNameEntry.empty().append("<option value='none'>&nbsp;</option>");
+            var lastName = lastNameEntry.val();
+            if( $.inArray(lastName, lastNames) < 0 )
+            {
+                // Didn't find last name in guest list
+                lastNameParent.append("<div class='errText textCentered mTop10'>No guests found.</div>");
+            }
+            else
+            {
+                allGuests.forEach(function(guest) {
+                   if( lastName === guest.l )
+                   {
+                       var fullName = guest.f + "." + guest.l;
+                        firstNameEntry.append($("<option></option>")
+                                                   .attr("value", fullName)
+                                                   .text(guest.f));
+                   }
+                });
+                $("#firstNameContainer").fadeIn(duration);
+            }
+        }
+    });
+
+    // Fires when someone selects their first name from the dropdown
+    firstNameEntry.on("change", function() {
+        var selectedName = firstNameEntry.find("option:selected").attr("value");
+
+        var handleGuestGroup = function() {
+            guestData.forEach(function(group) {
+                var curGroup = group;
+
+                group.forEach(function(guest) {
+                    var curGuestName = guest.f + "." + guest.l;
+                    if( curGuestName === selectedName )
+                    {
+                        curGroup.forEach(function(guest) {
+                           console.log(guest.f + " WOO " + guest.l);
+
+
+                        });
+
+                        // Don't want to process any more guests as guest was found
+                        return;
+                    }
+                });
+            });
+        };
+
+        handleGuestGroup();
+
+    });
+};
+
 // Attach event handlers and other things that require page being ready
 $(document).ready(function() {
 
@@ -45,7 +133,7 @@ $(document).ready(function() {
         passcodeSubmit();
     });
     $("#passcodeEntry").on("keydown", function(e) {
-        if(e.keyCode == 13)
+        if( e.keyCode == 13 )
         {
             passcodeSubmit();
         }
--- req/rsvpForm.php
+++ req/rsvpForm.php
@@ -1,13 +1,37 @@
+<?php
+    $jsonFile = "../data/guests.json";
+    if( file_exists($jsonFile) )
+    {
+        $jsonData = file_get_contents($jsonFile);
+        echo "<script type='text/javascript'>var guestData = $jsonData;</script>\n";
+    }
+    else
+    {
+        echo "<div class='errText'>Unable to find guest data.</div>\n";
+    }
+
+?>
+
 <div class='row'>
-    <div class='col-md-6'>
+    <div class='col-md-1'></div>
+    <div class='col-md-5'>
         <div class='formDesc textCentered'>
-            <p>Please enter <span class='fontReg'>your</span> last name.</p>
+            <p>Please type <span class='fontReg'>your</span> last name and hit enter.</p>
         </div>
         <div class='mTop10'>
-            <input class='dBlock centered' type='text' placeholder='Last Name'>
+            <input class='dBlock centered' id='lastNameEntry' type='text' placeholder='Last Name'>
         </div>
     </div>
-    <div class="col-md-6">
-
+    <div class='col-md-5'>
+        <div class='hideThis' id='firstNameContainer'>
+            <div class='formDesc textCentered'>
+                <p>Please select only <span class='fontReg'>your</span> first name.</p>
+            </div>
+            <div class='mTop10'>
+                <select class='dBlock centered' id='firstNameEntry'>
+                    <option value="default">&nbsp;</option>
+                </select>
+            </div>
+        </div>
     </div>
 </div>
